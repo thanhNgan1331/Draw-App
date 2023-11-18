c49b194a11c54ecf56ada1200298b8ee9564cf35
Phan Thành Ngân <nngann2402@gmail.com>
2023-11-18 10:34:19 +0700

    Update change imageview to drawview

---
+++ app/src/main/java/com/ptn/test_drawing/Stroke.java
@@ -0,0 +1,25 @@
+package com.ptn.test_drawing;
+
+import android.graphics.Path;
+
+public class Stroke {
+    // color of the stroke
+    public int color;
+
+    // width of the stroke
+    public int strokeWidth;
+
+    // a Path object to
+    // represent the path drawn
+    public Path path;
+
+    public int alpha;
+
+    // constructor to initialise the attributes
+    public Stroke(int color, int strokeWidth, Path path, int alpha) {
+        this.color = color;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+        this.alpha = alpha;
+    }
+}
